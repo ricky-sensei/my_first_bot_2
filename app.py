4ee4ba41843696580184b149ef4f31bf2903e976
ricky-sensei <axcelcamp.teacher@gmail.com>
2024-12-15 15:17:42 +0900

    second commit

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text + " リッキーさん"))
 
 
 if __name__ == "__main__":
